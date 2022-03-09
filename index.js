0e9d898d9e0bdeb2bcb5053f47fc9c2f22de8e69
Tom Nicholson <tfwnicholson+1@gmail.com>
2022-03-09 20:32:44 +0000

    Finish challenge

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 const imageFolder = "images";
 
-const randomImage = _ => {
-    let randomNumber = Math.floor(Math.random() * 6) + 1;
+const generateNumber = _ => Math.floor(Math.random() * 6) + 1;
+const getImagePath = number => imageFolder + "/" + `dice${number}.png`;
 
-    let imageName = `dice${randomNumber}.png`;
-    return imageFolder + "/" + imageName;
+const number1 = generateNumber();
+const image1 = getImagePath(number1);
+document.getElementsByClassName("img1")[0].setAttribute("src", image1);
+
+const number2 = generateNumber();
+const image2 = getImagePath(number2);
+document.getElementsByClassName("img2")[0].setAttribute("src", image2);
+
+let titleText = undefined;
+// Set title depending on who wins
+if (number1 < number2) {
+    titleText = "Player 2 wins 🚩";
+
+} else if (number1 > number2) {
+    titleText = "🚩 Player 1 wins";
+} else {
+    titleText = "Draw";
 }
-document.getElementsByClassName("img1")[0].setAttribute("src", randomImage());
-document.getElementsByClassName("img2")[0].setAttribute("src", randomImage());
+
+document.getElementsByTagName("h1")[0].textContent = titleText;
